981b31958e1c382d1fdd68a2aa398abc78ed5f62
Nate Glick <nxg9997@rit.edu>
2021-12-20 17:44:51 -0500

    modified host urls

--- extension/theater.js
+++ extension/theater.js
@@ -7,12 +7,12 @@
     openBtn.classList.add('ytp-button');
     openBtn.ariaLabel = 'Better YouTube Theater';
     openBtn.title = 'Better YouTube Theater';
-    openBtn.innerHTML = `<svg height="auto" version="1.1" viewBox="0 0 36 36" width="auto"><use class="ytp-svg-shadow"></use><image xlink:href="http://nozdormu.ng:8000/BYTT.svg" width="70%" height="70%" x="10%" y="10%"/></svg>`;
+    openBtn.innerHTML = `<svg height="auto" version="1.1" viewBox="0 0 36 36" width="auto"><use class="ytp-svg-shadow"></use><image xlink:href="https://nxg9997.github.io/BetterYTTheater/BYTT.svg" width="70%" height="70%" x="10%" y="10%"/></svg>`;
     
     openBtn.onclick = (e) => {
         let urlParams = new URLSearchParams(location.search);
         let vId = urlParams.get('v');
-        window.location = `http://nozdormu.ng:8000?v=${vId}`;
+        window.location = `https://nxg9997.github.io/BetterYTTheater?v=${vId}`;
     };
 
     info.appendChild(openBtn);
